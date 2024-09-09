0aee33269254f802804ad70db0b746f8c0cde185
Simon <Simon@LAPTOP-GFMLGC10>
2024-09-09 15:26:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 36;
+            if (myNumber > 30)
+            {
+                Console.WriteLine($"Wow, myNumber vilket stort tal, ditt tal var {myNumber}");
+            }
+            else if(myNumber < 40)
+            {
+                Console.WriteLine("Tal mindre än 40");
+            }
+            else
+            {
+                Console.WriteLine("Märkligt");
+            }
+
+        }
+    }
+}
